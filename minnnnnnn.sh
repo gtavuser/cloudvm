4d387e7f21fe7433477693bcb8bac519d785b4bb
adit das <gtavgame.r.0.0.9@gmail.com>
2024-06-02 10:54:09 +0530

    Update minnnnnnn.sh

--- minnnnnnn.sh
+++ minnnnnnn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt update
+sudo su
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
