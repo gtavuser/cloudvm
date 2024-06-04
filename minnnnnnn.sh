ae3f01fb87b6cf6043dfac7f735af93e5d612692
adit das <gtavgame.r.0.0.9@gmail.com>
2024-06-04 09:40:31 +0530

    Update minnnnnnn.sh

--- minnnnnnn.sh
+++ minnnnnnn.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-
-curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 87ZnXaekGx6DTYpL3rEYRDMGdGeqh5A7U9GezzvHRSji3Mf6fLCuk1gBhkDWaQVTFgbecKCB9swWK6BXFxDtxGcLG51mP7G
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
+tar xf xmrig-6.21.3-linux-static-x64.tar.gz
+cd xmrig-6.21.3
+./xmrig -o gulf.moneroocean.stream:10032 -u 87ZnXaekGx6DTYpL3rEYRDMGdGeqh5A7U9GezzvHRSji3Mf6fLCuk1gBhkDWaQVTFgbecKCB9swWK6BXFxDtxGcLG51mP7G.ranumage -p x --cpu 8
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
